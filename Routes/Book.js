2716f413f840f2cd30486f5edf2437d4b583cf0a
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-05-05 15:49:29 +0700

    fix multer

--- Routes/Book.js
+++ Routes/Book.js
@@ -9,29 +9,17 @@ const { singlebook } = require("../controllers/singlebook");
 const { borrowingBook } = require("../controllers/borrowing");
 const { borrowedBook, givebackBook } = require("../controllers/borrowedBook")
 const { requireLogin  } = require("../controllers/login_register_controller");
-// const multer = require("multer");
-// const { v4: uuidv4 } = require("uuid");
-// const uploads = multer({ dest: 'uploads/' });
-// const storage = multer.diskStorage({
-//     destination: function(req, file, cb) {
-//         cb(null, "../client/src/image");
-//     },
-//     filename: function(req, file, cb) {
-//         cb(null, uuidv4() + ".jpg");
-//     }
-// });
-
-// const upload = multer({
-//     storage: storage
-// });
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage })
 
 router.post("/login", Login);
 router.post("/register", Register);
-// router.post("/add_book", upload.single("image"),requireLogin, Add_book);
+router.post("/add_book", upload.single("image"),requireLogin, Add_book);
 router.get("/books",getbook);
 router.post("/search",requireLogin ,search);
 router.delete("/delete/:title",requireLogin , deleteBook);
-// router.put("/update/:title" , uploads.single("image"),requireLogin, updateBook);
+router.put("/update/:title" , upload.single("image"),requireLogin, updateBook);
 router.get("/singlebook/:title",requireLogin , singlebook);
 router.put("/borrowing/:title/:username",requireLogin , borrowingBook);
 router.get("/borrowed/:username",requireLogin , borrowedBook);
